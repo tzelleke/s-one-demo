373450f6df46aeb17ca7419e0c83d5dcf00a696d
tzelleke <theodros.zelleke@gmail.com>
2019-07-10 16:45:53 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <div id="nav">
+
+        <b-navbar toggleable="md">
+          <b-navbar-brand href="#">S-one Demo App</b-navbar-brand>
+
+          <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+          <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav>
+              <b-nav-item to="/">Home</b-nav-item>
+              <b-nav-item to="/about" disabled>About</b-nav-item>
+            </b-navbar-nav>
+          </b-collapse>
+        </b-navbar>
+
+    </div>
+    <router-view/>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="home">
+    <b-container class="bv-example-row">
+      <b-row>
+
+        <b-col cols="6" md="4" v-for="item in media">
+          <b-card
+              :title="item.title"
+              :img-src="item.thumbnailUrl"
+              img-top
+              class="mb-2"
+          >
+
+            <b-button href="#" variant="primary">Go somewhere</b-button>
+          </b-card>
+        </b-col>
+
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+    // @ is an alias to /src
+    // import HelloWorld from '@/components/HelloWorld.vue'
+    const ENDPOINT = 'https://jsonplaceholder.typicode.com'
+
+    export default {
+        name: 'home',
+        // components: {
+        //   HelloWorld
+        // }
+        data() {
+            return {
+                media: []
+            }
+        },
+        created() {
+            this.$http.get(ENDPOINT + '/photos')
+                .then(({data}) => this.media = data.slice(0, 20))
+                .then(console.log)
+        }
+    }
+</script>
